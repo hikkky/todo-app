f1cf6ab172a0873e2f74aa8f247b876cfefa3398
高木 弘輝 <h.takaki@hupro-inc.com>
2024-09-13 12:29:47 +0900

    Todo App作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { InputTodo } from './components/InputTodo';
+import { Title } from './components/Title';
+import { TodoList } from './components/TodoList';
+import { Task } from './components/Task';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title />
+      <Task todos={todos}/>
+      <InputTodo todos={todos} setTodos={setTodos}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
